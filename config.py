b2c231a1d31349e01924aa23ff73627710f07bc5
marlonhernandez01 <marlon.hernandez@allinsoftware.com>
2024-09-05 17:32:40 -0600

    Finalizacion, separacion botones

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://ADMINMH:Marlon123@PC-DEV36/Inventario_Vehiculos?driver=ODBC+Driver+17+for+SQL+Server'
+    #SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://ADMINMH:Marlon123@PC-DEV36/Inventario_Vehiculos?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://ADMIN:ADMIN@DESKTOP-HMS6GDC\\SQLEXPRESS/Inventario_Vehiculos?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
